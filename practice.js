8202fba16c183c6861623968df7a0d652ff12f69
kazzastic <rkazim79@gmail.com>
2019-03-26 20:13:53 +0500

    anything

--- practice.js
+++ practice.js
@@ -129,7 +129,7 @@ var emperor = new Emperor();
 
 assert(emperor === puppet , "The emperor is merely a puppet");
 assert(emperor.rules === false, "The puppet does not know how to rule");
-
+console.log("anything");
 
 
 
