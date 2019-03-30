225b859fa6fc1db9daf0b77ab07ad29c8420a21f
kazzastic <rkazim79@gmail.com>
2019-03-31 03:13:00 +0500

    React

---
+++ hello-world/src/App.js
@@ -0,0 +1,16 @@
+import React, { Component } from 'react';
+//import logo from './logo.svg';
+import './App.css';
+
+class App extends Component {
+  render() {
+    return (
+      <div className="App">
+      <h1>Hello World!</h1>
+      <h2>This is my first React App</h2>
+      </div>
+    );
+  }
+}
+
+export default App;
