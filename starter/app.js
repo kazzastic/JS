92d0473a0270ee12b0d8cae86058cf6aa5673ad4
kazzastic <rkazim79@gmail.com>
2019-05-25 13:38:49 +0500

    dice game

--- starter/app.js
+++ starter/app.js
@@ -8,3 +8,53 @@ GAME RULES:
 - The first player to reach 100 points on GLOBAL score wins the game
 
 */
+
+//Declerations of globally used var's
+var player_one_name = document.querySelector('#name-0');
+var player_two_name = document.querySelector('#name-1');
+
+var player_one = document.querySelector('#current-0');
+var player_two = document.querySelector('#current-1');
+
+var player_one_score = document.querySelector('#score-0');
+var player_two_score = document.querySelector('#score-1');
+
+var img = document.querySelector('.dice');
+
+var btn;
+
+var player_one_total = 0;
+var player_two_total;
+
+//Intialization of globally declared var's
+img.style.display = 'none';
+
+player_one_score.textContent = 0;
+player_two_score.textContent = 0;
+
+player_one.textContent = 0;
+player_two = 0;
+
+player_one_name.textContent = "kazim";
+player_two_name.textContent = 'Server';
+
+btn = document.querySelector('.btn-roll').addEventListener('click', function(){
+	var rand_num = player_one.textContent = Math.floor(Math.random()*6)+1;	
+	var temp;
+	img.style.display = 'block';
+	img.src = 'dice-'+rand_num+'.png';
+	player_one_total = player_one_total + rand_num;
+	player_one_score.textContent = player_one_total;
+	if (rand_num === 1) {
+		img.style.display = 'none';
+	}
+});
+
+
+
+
+
+
+
+
+
