7a4538bd8369cfd242d96cd6c07aa51b37842075
kazzastic <rkazim79@gmail.com>
2019-06-27 01:09:30 +0500

    the sort of twitter styled app

--- react/contacts/src/App.js
+++ react/contacts/src/App.js
@@ -1,13 +1,36 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, {Component} from 'react';
+import ListContacts from './ListContacts'
 
-function App() {
-  return (
-    <div className="App">
-      <h1>Kaszim</h1>
-    </div>
-  );
+const contacts = [
+  {
+    "id": "karen",
+    "name": "Karen Isgrigg",
+    "handle": "karen_isgrigg",
+    "avatarURL": "http://localhost:5001/karen.jpg"
+  },
+  {
+    "id": "richard",
+    "name": "Richard Kalehoff",
+    "handle": "richardkalehoff",
+    "avatarURL": "http://localhost:5001/richard.jpg"
+  },
+  {
+    "id": "tyler",
+    "name": "Tyler McGinnis",
+    "handle": "tylermcginnis",
+    "avatarURL": "http://localhost:5001/tyler.jpg"
+  }
+ ];
+
+class App extends Component{
+  render(){
+    return(
+      <div>
+        <ListContacts contacts={contacts} />
+      </div>
+    )
+  }
 }
+
 
 export default App;
