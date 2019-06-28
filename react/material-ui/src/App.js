f84b1212c34aa75758e5fd5595900ec5e784004a
kazzastic <rkazim79@gmail.com>
2019-06-29 03:08:08 +0500

    google maps and react-materialUI

---
+++ react/material-ui/src/App.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import {Component} from 'react'
+import SimpleMap from './components/NavBar';
+
+class App extends Component{
+  render(){
+    return(
+      <SimpleMap/>
+    )
+  }
+}
+
+export default App;
---
+++ react/material-ui/src/components/CourseList.js
@@ -0,0 +1,6 @@
+import React from 'react'
+import {Component} from 'react'
+import Grid from '@material-ui/core/Grid'
+import TextField from '@material-ui/core/TextField'
+import * as contentful from contentful 
+import Course from '../components/Course'
